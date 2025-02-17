102d2fb5db6454ce44a9ae9492ee6cf9fa73ac41
Choi Phil <philchoi@icloud.com>
2025-02-17 12:24:41 +0900

    adding random password gen function

--- exercises/math.exercises.js
+++ exercises/math.exercises.js
@@ -1,49 +1,66 @@
 // Exercise 1: Generate a random number between 0 and 1
 function randomZeroToOne() {
   // Your code here
+  return Math.random();
 }
+console.log(randomZeroToOne());
 
 // Exercise 2: Generate a random integer between two values, inclusive
 function randomIntBetween(min, max) {
   // Your code here
+
+  // 1. get random number between 0 and max - min
+  const num = Math.floor(Math.random() * (max - min));
+  // 2. add min to that total
+  return num + min;
 }
+
+console.log(randomIntBetween(1, 30));
 
 // Exercise 3: Calculate the square root of a number
 function calculateSquareRoot(num) {
   // Your code here
+  return Math.sqrt(num);
 }
+
+console.log(calculateSquareRoot(25));
 
 // Exercise 4: Calculate the power of a number
 function calculatePower(base, exponent) {
   // Your code here
+  return Math.pow(base, exponent);
 }
+
+console.log(calculatePower(2, 2));
 
 // Exercise 5: Round a number to the nearest integer
 function roundNumber(num) {
   // Your code here
+  return Math.round(num);
 }
+
+console.log(roundNumber(4.4));
 
 // Exercise 6: Find the maximum number in an array
 function findMax(arr) {
   // Your code here
+  return Math.max(...arr);
 }
+
+console.log(findMax([1, 2, 3, 4, 5]));
 
 // Exercise 7: Find the minimum number in an array
 function findMin(arr) {
   // Your code here
+  return Math.min(...arr);
 }
+
+console.log(findMin([1, 2, 3, 4, 5]));
 
 // Exercise 8: Calculate the absolute value of a number
 function calculateAbsoluteValue(num) {
   // Your code here
+  return Math.abs(num);
 }
 
-// Exercise 9: Calculate the sine of an angle (in radians)
-function calculateSine(angle) {
-  // Your code here
-}
-
-// Exercise 10: Calculate the cosine of an angle (in radians)
-function calculateCosine(angle) {
-  // Your code here
-}
+console.log(calculateAbsoluteValue(-4));
---
+++ exercises/random-pw-gen.js
@@ -0,0 +1,71 @@
+// random chars to be used
+const chars =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-=_+1234567890";
+
+function randomIdx(length) {
+  return Math.floor(Math.random() * length);
+}
+
+function getRandomPassword(length, randomGen, cs) {
+  // const clonedChar = chars.split("");
+  // let shuffle = "";
+
+  // for (let i = 0; i < length; i++) {
+  //   const [char] = clonedChar.splice(randomIdx(clonedChar.length), 1);
+  //   shuffle += char;
+  // }
+
+  // return shuffle;
+
+  let pw = "";
+  // O(8)
+  for (let i = 0; i < length; i++) {
+    pw += cs[randomGen(chars.length)];
+  }
+  return pw;
+}
+
+// const eight = getRandomPassword(8, randomIdx, chars);
+// console.log("eight =", eight);
+// const ten = getRandomPassword(10, randomIdx, chars);
+// console.log("ten = ", ten);
+// const twelve = getRandomPassword(12, randomIdx, chars);
+// console.log("twelve =", twelve);
+
+/**
+ * @definition make a function that gives random character given a string as a paremter
+ * @param {String} chars - string to get random character from
+ * @returns single character string
+ */
+function getRandomChar(chars) {
+  // let str = "";
+  // for (let i = 0; i < chars.length; i++) {
+  //   console.log("i =", i);
+  //   if (chars[i] !== " ") str += chars[i];
+  // }
+
+  // return str;
+
+  // gettign random number between 0 - (chars.length - 1) and getting chars[on that index]
+  const randomChar = chars[Math.floor(Math.random() * chars.length)];
+  // check if that random index char is empty string
+  if (randomChar === " ") {
+    // if it is. return function with same string
+    return getRandomChar(chars);
+  } else return randomChar;
+}
+
+const randomChar = getRandomChar(
+  "Phil Choi David Jocelin William Mustafa Colby Minnie"
+);
+console.log("randomChar =", randomChar);
+
+// let num = 0;
+
+// function addToNum(num) {
+//   console.log("num =", num);
+//   // if (num === 5) return console.log("we got to five");
+//   return addToNum(num + 1);
+// }
+
+// addToNum(num);
